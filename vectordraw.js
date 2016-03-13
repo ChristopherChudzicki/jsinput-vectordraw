600a8b8465c6829d722ee2c4076b9b32a3fbcf0f
Chris Chudzicki <christopher.chudzicki@gmail.com>
2016-03-13 13:10:02 -0400

    change conditional to check if variable exists
    The original version, `if (selected.idx ...)` does not behave correctly when `selected.idx` has value `0`. This caused "click to draw" to fail for first object (object `0`).

--- vectordraw.js
+++ vectordraw.js
@@ -458,7 +458,7 @@ VectorDraw.prototype.onBoardDown = function(evt) {
     var selected = this.getSelectedElement();
     var coords = this.getMouseCoords(evt);
     var targetObjects = this.objectsUnderMouse(coords);
-    if (selected.idx && _.all(targetObjects, this.canCreateVectorOnTopOf.bind(this)) ) {
+    if ( typeof(selected.idx) !== undefined && _.all(targetObjects, this.canCreateVectorOnTopOf.bind(this)) ) {
         var point_coords = [coords.usrCoords[1], coords.usrCoords[2]];
         if (selected.type === 'vector') {
             this.drawMode = true;
